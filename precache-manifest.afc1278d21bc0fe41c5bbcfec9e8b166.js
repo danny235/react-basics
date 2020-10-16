44f4b0c551f2dff845f0174b51ad6f23b4c015b3
danny235 <danielbarima235@gmail.com>
2020-10-16 20:37:26 +0100

    Updates

---
+++ precache-manifest.afc1278d21bc0fe41c5bbcfec9e8b166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360709117011ce7b606cdb4a626a4927",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "891485f2d432e93f76d5",
+    "url": "/react-todo/static/css/main.6d48b7fa.chunk.css"
+  },
+  {
+    "revision": "41719ea6d793b8891aca",
+    "url": "/react-todo/static/js/2.19a5f12f.chunk.js"
+  },
+  {
+    "revision": "ce53a315179bf1b31de3d98da26d8898",
+    "url": "/react-todo/static/js/2.19a5f12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891485f2d432e93f76d5",
+    "url": "/react-todo/static/js/main.70ba6991.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
